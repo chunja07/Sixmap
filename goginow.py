36a33ee3b1bcf10236ae4912b074f4ffaeff427c
Nak <35492393+chunja07@users.noreply.github.com>
2021-01-22 01:52:37 +0900

    Update goginow.py

--- goginow.py
+++ goginow.py
@@ -34,37 +34,26 @@ st.write('''
 ●설명 : 국내 지역 및 전 세계 나라 기반 육류 소개 및 온라인 판매
 
 ●배경 : 
-
 1. 육류 상품은 넘쳐나지만, 방향성을 상실한 풍요 속의 빈곤 속에서 자신만의 고기를 찾을 수 있는 나침반 필요
 2. 타사 온라인 정육 사이트와 차별성 필요
 3. 추후 지역 농가의 소개 및 홍보 창구 역할을 수행함으로써 지역 농가 활성이라는 사회적 메세지 전달
 4. 육그램이 육류 업계에서 남기고자 하는 이상적 의미 정립 및 방향성 확립
-
-
 '''
 )
 
-
-for data in values:
-   
-   temp1 = {
-       '나라':data[0],
-       '지역':data[1],
-       '부위':data[2] 
-   }
-   temp2 = pd.DataFrame(temp1)
-   nation = pd.concat(temp2)
-
-print(nation)
-
 np_values = np.array(values)
 
-data_frame = pd.DataFrame({
-'나라': values[0],
-'지역' : values[1],
-'부위' : values[2]
-})
+df = pd.DataFrame(np_values, columns=['나라','지역','부위'])
 
+st.table(df)
+
+#st.write(np_values)
+#st.write(np_values.dtype)
+
+print(np_values)
+
+
+'''
 st.write('Below is a DataFrame : ', data_frame)
 
 df = pd.DataFrame(
@@ -75,3 +64,5 @@ c = alt.Chart(df).mark_circle().encode(
      x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
 
 st.write('below is a air condition graph',df,c)
+'''
+
